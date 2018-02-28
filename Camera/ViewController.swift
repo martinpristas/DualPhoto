5e2e989dfed9a05bb10f96c7281c9e286613a970
Martin Pristas <xprist02@stud.fit.vutbr.cz>
2018-02-28 09:23:28 +0100

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Martin Pristas on 28.2.18.
+//  Copyright © 2018 Martin Pristas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
